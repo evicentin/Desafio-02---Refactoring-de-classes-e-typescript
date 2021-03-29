fa1aaa52befc476a14b70af0e49a3b639697782f
Ewerton <ewerton@evconsultoria.com>
2021-03-29 16:59:38 -0300

    Resultado

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,4 +1,5 @@
-import styled, { css } from 'styled-components';
+/* eslint-disable no-mixed-operators */
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   display: flex;
@@ -9,30 +10,32 @@ export const Container = styled.div`
   padding: 18px 24px;
   width: 100%;
   font-size: 16px;
+  border: 2px solid #fff;
+  color: #b7b7cc;
 
   & + div {
     margin-top: 24px;
   }
 
+  ${(props) =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+      color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+      
   h1 {
     margin-bottom: 40px;
     font-weight: 600;
     font-size: 36px;
     line-height: 36px;
   }
-
-  ${props =>
-    props.isFocused &&
-    css`
-      color: #ff9000;
-      border-color: #ff9000;
-    `}
-
-  ${props =>
-    props.isFilled &&
-    css`
-      color: #ff9000;
-    `}
 
   input {
     flex: 1;
---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,56 @@
+import { createRef } from "react";
+import { FiCheckSquare } from "react-icons/fi";
+
+import { Form } from "./styles";
+import Modal from "../Modal";
+import Input from "../Input";
+
+interface FoodInput {
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string;
+}
+
+interface ModalProps {
+  isOpen: boolean;
+  editingFood: FoodInput;
+  setIsOpen: () => void;
+  handleUpdateFood: (data: FoodInput) => void;
+}
+
+export default function ModalEditFood({
+  isOpen,
+  setIsOpen,
+  editingFood,
+  handleUpdateFood,
+}: ModalProps) {
+  const formRef = createRef();
+
+  const handleSubmit = async (data: FoodInput) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
